e554fa3223c534fa949bed4518cb706d1df26130
Kriscoboss711 <kriscohristov77@gmail.com>
2025-01-13 22:50:26 +0200

    comment added

---
+++ languageCheck.js
@@ -0,0 +1,19 @@
+function langueageSpoken(country){
+    if(country == "USA" || country == "England"){
+        console.log("English")
+    }else if(country == "Spain" || country == "Argentina" || country == "Mexico"){
+        console.log("Spanish");
+    }else{
+        console.log("unknown");
+    }
+}
+
+langueageSpoken("USA");
+langueageSpoken("Argentina");
+langueageSpoken("Mexico");
+langueageSpoken("Germany");
+langueageSpoken("albania");
+langueageSpoken("bulgaria")
+langueageSpoken("france")
+langueageSpoken("portugal")
+
---
+++ theatreTickets.js
@@ -0,0 +1,43 @@
+function calculateTickets(day,age){
+    if(day == "Weekday"){
+        if(age >=0 && age <= 18){
+            console.log("12$")
+        }
+        if(age >18 && age <= 64){
+            console.log("18$")
+        }
+        if(age >64 && age <= 122){
+            console.log("12$")
+        }
+    }else if(day == "Weekend"){
+        if(age >=0 && age <= 18){
+            console.log("15$")
+        }
+        if(age >18 && age <= 64){
+            console.log("20$")
+        }
+        if(age >64 && age <= 122){
+            console.log("15$")
+        }
+    }else if(day == "Holiday"){
+        if(age >=0 && age <= 18){
+            console.log("5$")
+        }
+        if(age >18 && age <= 64){
+            console.log("12$")
+        }
+        if(age >64 && age <= 122){
+            console.log("10$")
+        }
+    }else{
+        console.log("Error...")
+    }
+}
+
+calculateTickets("Holiday",14);
+calculateTickets("Weekend",44);
+calculateTickets("Weekend",82);
+calculateTickets("Weekday",92);
+calculateTickets("blah blah",14);
+//
+
